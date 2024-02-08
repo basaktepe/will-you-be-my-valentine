067d4c602e1a611cefc48bde0a903bad5d6c8dae
Başak Tepe <100874056+basaktepe@users.noreply.github.com>
2024-02-08 21:31:43 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
-const yesButtonElement = document.getElementById("yes");
-const noButtonElement = document.getElementById("bouncing");
-const titleElement = document.querySelector("h1");
-
 const buttonHeight = 50;
 const buttonWidth = 150;
 const maxWidth = window.innerWidth - buttonWidth;
 const maxHight = window.innerHeight - buttonHeight;
 
-yesButtonElement.addEventListener("click", yesClicked);
-noButtonElement.addEventListener("mouseover", noHovered);
+window.addEventListener("DOMContentLoaded", () => {
+  const yesButtonElement = document.getElementById("yes");
+  const noButtonElement = document.getElementById("bouncing");
+  function yesClicked() {
+    titleElement.textContent =
+      "Congratulations, you have a beautiful girlfriend like me.";
+    yesButtonElement.style.display = "none";
+    noButtonElement.style.display = "none";
+  }
 
-function yesClicked() {
-  titleElement.textContent =
-    "Congratulations, you have a beautiful girlfriend like me.";
-  yesButtonElement.style.display = "none";
-  noButtonElement.style.display = "none";
-}
+  function noHovered() {
+    noButtonElement.style.left =
+      Math.floor(Math.random()) * (maxWidth + 1) + "px";
+    noButtonElement.style.top =
+      Math.floor(Math.random()) * (maxHight + 1) + "px";
+  }
 
-function noHovered() {
-  noButtonElement.style.left =
-    Math.floor(Math.random()) * (maxWidth + 1) + "px";
-}
+  const titleElement = document.querySelector("h1");
+  yesButtonElement.addEventListener("click", yesClicked);
+  noButtonElement.addEventListener("mouseover", noHovered);
+});
