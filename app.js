56f1fca6278d2a564d99142a72a1d670b6a0cb14
Başak Tepe <100874056+basaktepe@users.noreply.github.com>
2024-02-08 22:12:51 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,47 +1,24 @@
-body {
-  background-color: hsla(332, 90%, 91%, 0.66);
-  font-family: "Single Day", sans-serif;
-  margin: auto;
-  text-align: center;
+const buttonHeight = 50;
+const buttonWidth = 150;
+const maxWidth = window.innerWidth - buttonWidth;
+const maxHight = window.innerHeight - buttonHeight;
+
+const yesButtonElement = document.getElementById("yes");
+const noButtonElement = document.getElementById("bouncing");
+const titleElement = document.querySelector("h1");
+
+function yesClicked() {
+  titleElement.textContent =
+    "Congratulations, you have a beautiful girlfriend like me.";
+  yesButtonElement.style.display = "none";
+  noButtonElement.style.display = "none";
 }
 
-h1 {
-  font-size: 5rem;
-  text-align: center;
-  color: rgb(88, 150, 198);
+function noHovered() {
+  noButtonElement.style.left =
+    Math.floor(Math.random() * (maxWidth + 1)) + "px";
+  noButtonElement.style.top = Math.floor(Math.random() * (maxHight + 1)) + "px";
 }
 
-#dudu {
-  max-width: 15rem;
-}
-
-#buttons {
-  text-align: center;
-  margin-top: 2rem;
-}
-
-button {
-  font-family: inherit;
-  width: 150px;
-  height: 50px;
-  padding: 5px;
-  margin-right: 20px;
-
-  border-radius: 5px;
-  color: rgb(61, 60, 60);
-}
-
-#yes {
-  background-color: rgb(32, 132, 32);
-  border-color: rgb(32, 132, 32);
-}
-
-#bouncing {
-  background-color: red;
-  border-color: red;
-  position: absolute;
-}
-
-.first {
-  width: 20%;
-}
+yesButtonElement.addEventListener("click", yesClicked);
+noButtonElement.addEventListener("mousemove", noHovered);
